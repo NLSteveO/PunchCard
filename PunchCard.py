4590efd254502d0c64e1c73aa8a6712736e00812
Steve O'Keefe <steveokeefe88@gmail.com>
2017-01-21 16:04:41 -0330

    Add the Punch Card program and test input file

---
+++ PunchCard.py
@@ -0,0 +1,46 @@
+import fileinput
+
+weekHours = 0.0
+dayHours = 0.0
+
+def calcWorkTime(timeIn, timeOut):
+  inSplit = timeIn.split(':')
+  outSplit = timeOut.split(':')
+  hourIn = int(inSplit[0])
+  minuteIn = int(inSplit[1])
+  hourOut = int(outSplit[0])
+  minuteOut = int(outSplit[1])
+
+  if hourIn > hourOut:
+    newHour = (hourOut + 24) - hourIn
+  else:
+    newHour = hourOut - hourIn
+
+  newMinutes = minuteOut - minuteIn
+  if newMinutes < 0:
+    newHour -= 1
+    newMinutes += 60
+
+  global dayHours
+  dayHours += float(newHour) + (float(newMinutes)/60)
+
+def calculateDay(dayEntry):
+  day = dayEntry.pop(0)[0]
+  index = 0
+  while(index < len(dayEntry)):
+    calcWorkTime(dayEntry[index], dayEntry[index+1])
+    index += 2
+  print day + ': ' + str(dayHours)
+
+
+lines=[]
+for line in fileinput.input():
+  lines.append(line)
+
+print lines.pop(0)
+for line in lines:
+  calculateDay(line.split(','))
+  weekHours += dayHours
+  dayHours = 0
+
+print '\nTotal hours for the week: ' + str(weekHours)
