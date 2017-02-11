2101453d1bf1de0858f9693fa801deb296820195
Steve O'Keefe <steveokeefe88@gmail.com>
2017-02-11 11:07:38 -0330

    Add argparse library and set flags for program
    Import argparse
    Add a Argument Parser with a description.
    Add a mutually exclusive group.
    Add to the group two optional arguments H(hours) and m(minutes) with the action "store_true".
    Add a positional argument file with type FileType('r') default set to '-' which is used for stdin so that when there is no file given to read it will read from stdin instead.
    After calling parse_args check the file argument to see if it is reading from stdin or a file
    Add logic to check which flag was used, H, m, None
    Give the main function the array of lines read and the timeFormat which was decided based on the flags used if any.
    
    Add new function printDayHours which takes a string for the day represented as a letter, a number which is the number of hours for that day, and the timeFormat. The time is calculated as decimal hours and int hours with int minutes. Then it decides which timeFormat is given and prints to screen based on that. The print line for each day in the main function is replaced with a call to this function instead.
    
    Add new function printWeekHours which much like the printDayHours function takes the number of hours and timeFormat. Calculates the hours in different ways the same way. Decides the format the same way and then prints to screen accordingly. The print line for the week is replaced in the main function with a call to this function.

--- PunchCard.py
+++ PunchCard.py
@@ -1,3 +1,4 @@
+import argparse
 import fileinput
 
 
@@ -31,21 +32,66 @@ def calculateDay(dayEntry):
     return dayHours
 
 
-def main(input):
+def printDaysHours(day, hours, timeFormat):
+    floatHours = round(hours, 3)
+    intHours = int(floatHours)
+    intMinutes = int((floatHours - intHours)*60)
+    if timeFormat == 'HH.hhh':
+        print('{}: {} hours'.format(day, floatHours))
+    elif timeFormat == 'HH:mm':
+        print('{}: {} hours {} minutes'.format(day, intHours, intMinutes))
+    else:
+        print('{}: {} hours {} minutes({} hours)'.format(day, intHours, intMinutes, floatHours))
+
+
+def printWeekHours(hours, timeFormat):
+    floatHours = round(hours, 3)
+    intHours = int(floatHours)
+    intMinutes = int((floatHours - intHours)*60)
+    if timeFormat == 'HH.hhh':
+        print('\nTotal hours for the week: {} hours'.format(floatHours))
+    elif timeFormat == 'HH:mm':
+        print('\nTotal hours for the week: {} hours {} minutes'.format(intHours, intMinutes))
+    else:
+        print('\nTotal hours for the week: {} hours {} minutes({} hours)'.format(intHours, intMinutes, floatHours))
+
+
+def main(input, timeFormat):
     weekHours = 0.0
     print(input.pop(0))
     for line in input:
         daySplit = line.split(',')
         dayLetter = daySplit.pop(0)[0]
         dayHours = calculateDay(daySplit)
-        print(dayLetter + ': ' + str(dayHours))
+        printDaysHours(dayLetter, dayHours, timeFormat)
         weekHours += dayHours
 
-    print('\nTotal hours for the week: ' + str(weekHours))
+    printWeekHours(weekHours, timeFormat)
 
 
 if __name__ == '__main__':
+    description = 'This is a simple program meant to calculate my work hours.'
+    parser = argparse.ArgumentParser(description=description)
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-H', '--hours', action='store_true', help='Outputs hours only as a decimal')
+    group.add_argument('-m', '--minutes', action='store_true', help='Outputs hours and minutes')
+    parser.add_argument('file', nargs='?', type=argparse.FileType('r'), default='-',
+                        help='The file to be read or if no file given read stdin')
+    args = parser.parse_args()
+
     lines = []
-    for line in fileinput.input():
-        lines.append(line)
-    main(lines)
+    if args.file.name == '<stdin>':
+        for line in fileinput.input('-'):
+            lines.append(line)
+    else:
+        for line in fileinput.input(args.file.name):
+            lines.append(line)
+
+    if args.hours:
+        timeFormat = 'HH.hhh'
+    elif args.minutes:
+        timeFormat = 'HH:mm'
+    else:
+        timeFormat = None
+
+    main(lines, timeFormat)
